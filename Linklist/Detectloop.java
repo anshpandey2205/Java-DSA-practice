c1d5763a9aeb3795f3381a5b6b7139668047f7e8
anshpandey2205 <anshupandey2227@gmail.com>
2025-10-31 21:50:07 +0530

    check

---
+++ Linklist/Detectloop.java
@@ -0,0 +1,73 @@
+public class Detectloop {
+    public static class node{
+        int data;
+        node next;
+
+        public node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static node head;
+    public static node tail;
+
+    public void addFirst(int data){
+        // STEP 1 = CREATE NEW NODE
+        node newNode=new node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+
+        // STEP 2 = NEWNODE NEXT= TEMP
+        newNode.next=head;
+
+        // STEP 3 = HEAD = NEWNODE
+        head=newNode;
+    }
+
+    public void addLast(int data){
+        node newNode=new node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+
+    public void print(){
+        node temp=head;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+        //System.out.println();
+    }
+               
+                    // DETECT A LOOP OR CYCLE
+
+    public static boolean check(){
+        node slow=head;
+        node fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }                
+
+    public static void main(String[] args) {
+        head=new node(1);
+        head.next=new node(2);
+        head.next.next=new node(3);
+        head.next.next.next=head;
+        System.out.println("detecting loop");
+        System.out.println(check());
+       
+    }
+}
