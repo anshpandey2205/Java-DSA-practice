b7ca88aadd87fecf5a83e09468acc88580c8e9ce
anshpandey2205 <anshupandey2227@gmail.com>
2025-12-01 20:50:27 +0530

    recursion

--- Recurrsion/decreaseOrder.java
+++ Recurrsion/decreaseOrder.java
@@ -9,6 +9,7 @@ public class decreaseOrder {
         decreasenumber(n-1);
     }
     public static void main(String[] args) {
+        System.out.println("Decreasing order:");
         decreasenumber(10);
     }
 }
