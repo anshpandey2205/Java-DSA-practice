5f119b26913c78d5a24de13ca94eb0bb3a08a8b6
anshpandey2205 <anshupandey2227@gmail.com>
2025-12-01 20:50:59 +0530

    recursion

--- Recurrsion/increaseOrder.java
+++ Recurrsion/increaseOrder.java
@@ -8,6 +8,7 @@ public class increaseOrder {
         System.out.println(n);
     }
     public static void main(String[] args) {
+        System.out.println("Increasing order:");
         printIncrease(10);
     }
 }
