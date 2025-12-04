676e57b83ca5dc1475bdb08fabcae420c4ed0849
anshpandey2205 <anshupandey2227@gmail.com>
2025-12-04 22:40:56 +0530

    recurrsion

--- Recurrsion/binaryString.java
+++ Recurrsion/binaryString.java
@@ -1,3 +1,16 @@
 public class binaryString {
-    
+    public static void printbinString(int n,int lastdigit,String str){
+        // Base case
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
+        printbinString(n-1,0, str+"0");
+        if(lastdigit==0){
+            printbinString(n-1,1, str+"1");
+        }
+    }
+    public static void main(String[] args) {
+        printbinString(3, 0, "");
+    }
 }
